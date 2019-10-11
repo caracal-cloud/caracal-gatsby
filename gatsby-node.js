06ccee667e2e35e39edbc2061927b7805602a194
Pedro Nauck <pedronauck@gmail.com>
2019-10-10 21:08:14 -0300

    fix: route paths

--- gatsby-node.js
+++ gatsby-node.js
@@ -2,7 +2,7 @@ const pages = require('./src/data/pages.json')
 
 exports.onCreatePage = ({ page, actions }) => {
   const { createPage, deletePage } = actions
-  const path = page.path.slice(0, -1)
+  const path = page.path.slice(0)
   const item = pages.find(page => page.path === path)
 
   if (item) {
